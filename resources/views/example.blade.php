53e48983508c9c2b162dff355d12ac1e8ecfa857
Rafael-cmd-html <armandocastillejos086@gmail.com>
2023-11-05 18:59:48 -0700

    Primeras rutas terminadas

---
+++ resources/views/example.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+<body>
+    <h1>Hola {{ $name }}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Aut maxime at ipsum delectus repellat laborum quidem esse, 
+         nemo aspernatur? Sed hic doloribus consectetur, 
+         nobis iure excepturi sint repudiandae labore pariatur?</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Saludos-Rutas terminadas
+Route::get('/greeting/{name?}', function ($name="Jhon") {
+    return view('example', ['name'=> $name]);
+})->where(['name' => '[a-zA-Z]+']); //Valido tmb para poner la mayuscula al inicio
+
+Route::get('/saludo/{nombre}/{lastname?}', function ($nombre,$lastname="Doe") {
+    return 'Hola ' .$nombre.' '.$lastname;
+})->where(['nombre' => '[a-zA-Z]+', 'lastname'=> '[a-zA-Z]+']);
+
+
+//Operaciones-Rutas terminadas
+
+Route::get('/suma/{n1}/{n2}', function ($n1,$n2) {
+    return $n1 + $n2;
+})->where(['n1'=>'[0-9]', 'n2'=>'[0-9]']);
+Route::get('/resta/{n1}/{n2}', function ($n1,$n2) {
+    return $n1 - $n2;
+})->where(['n1'=>'[0-9]', 'n2'=>'[0-9]']);
+Route::get('/multiplicacion/{n1}/{n2}', function ($n1,$n2) {
+    return $n1 * $n2;
+})->where(['n1'=>'[0-9]', 'n2'=>'[0-9]']);
+Route::get('/division/{n1}/{n2}', function ($n1,$n2) {
+    return $n1 / $n2;
+})->where(['n1'=>'[0-9]', 'n2'=>'[0-9]']);
